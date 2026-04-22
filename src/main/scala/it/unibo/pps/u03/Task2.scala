7df7b1d3a9fcb20ed4603aa2fd2c3569baf0d8b7
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2026-04-22 17:20:34 +0200

    feat: foldLeft development of task2

--- src/main/scala/it/unibo/pps/u03/Task2.scala
+++ src/main/scala/it/unibo/pps/u03/Task2.scala
@@ -11,3 +11,13 @@ object Task2:
   def getTeacherCourses(s: Sequence[Person]): Sequence[String] =
     //map(filter(s) { case Teacher(_, _) => true; case _ => false }) { case Teacher(_, c) => c; case _ => "" }
     flatMap(s) { case Teacher(_, c) => Cons(c, Nil()); case _ => Nil() }
+
+
+  def foldLeft[A, B](s: Sequence[A])(initial: B)(opt: (B, A) => B): B =
+
+    @scala.annotation.tailrec
+    def _optLeft(current: Sequence[A], acc: B): B = current match
+      case Cons(h, t) => _optLeft(t, opt(acc, h))
+      case Nil() => acc
+
+    _optLeft(s, initial)  
--- src/test/scala/it/unibo/pps/u03/Task2Test.scala
+++ src/test/scala/it/unibo/pps/u03/Task2Test.scala
@@ -11,8 +11,14 @@ class Task2Test:
   import it.unibo.pps.u02.AlgebraicDataTypes.Person
   import it.unibo.pps.u02.AlgebraicDataTypes.Person.*
 
-  val sequence: Sequence[Person] = Cons(Student("mario", 2020), Cons(Teacher("luigi", "Programming"),
+  val sequencePerson: Sequence[Person] = Cons(Student("mario", 2020), Cons(Teacher("luigi", "Programming"),
     Cons(Student("maria", 2021), Cons(Teacher("laura", "Statistics"), Nil()))))
 
+  val lst = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+
   @Test def testGetTeacherCourses(): Unit =
-    assertEquals(Cons("Programming", Cons("Statistics", Nil())), getTeacherCourses(sequence))
+    assertEquals(Cons("Programming", Cons("Statistics", Nil())), getTeacherCourses(sequencePerson))
+
+  @Test def testFoldLeft(): Unit =
+    assertEquals(-16, foldLeft(lst)(0)(_ - _))
+    assertEquals(105, foldLeft(lst)(1)(_ * _))
