d59dcc7f35c353825a8367d1bbdc7934a769ea6d
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2026-04-22 16:17:09 +0200

    feat: getTeacherCourses development with flatMap

--- src/main/scala/it/unibo/pps/u03/Task2.scala
+++ src/main/scala/it/unibo/pps/u03/Task2.scala
@@ -9,4 +9,5 @@ object Task2:
   import it.unibo.pps.u02.AlgebraicDataTypes.Person.*
 
   def getTeacherCourses(s: Sequence[Person]): Sequence[String] =
-    map(filter(s) { case Teacher(_, _) => true; case _ => false }) { case Teacher(_, c) => c; case _ => "" }
+    //map(filter(s) { case Teacher(_, _) => true; case _ => false }) { case Teacher(_, c) => c; case _ => "" }
+    flatMap(s) { case Teacher(_, c) => Cons(c, Nil()); case _ => Nil() }
